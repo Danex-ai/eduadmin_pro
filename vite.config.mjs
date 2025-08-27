db54903fa0a080a2a064decea9c72b7152e41e91
www.samrainbow <samuelogundipe26@gmail.com>
2025-08-27 11:45:33 +0100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,9 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/eduadmin_pro/",
+  plugins: [react()],
+});
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
